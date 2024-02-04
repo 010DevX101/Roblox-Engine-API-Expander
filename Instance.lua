50abffba5c60db645a344a7ad40780395d288fa8
DevX <63361968+010DevX101@users.noreply.github.com>
2024-02-03 23:28:59 -0300

    Added more functions

--- Instance.lua
+++ Instance.lua
@@ -8,21 +8,21 @@ function instance:WaitForChildWhichIsA(i : Instance, className : string, timeOut
 	while task.wait(1) do
 		if timeOut <= 0 then
 			warn("Infinite yield possible on " .. "'" .. i.Name .. ':WaitForChildWhichIsA("' .. className .. '")' .. "'")
-			break	
+			break
 		end
-		timeOut -= 1
 		for _,inst in pairs(i:GetChildren()) do
 			if inst:IsA(className) then
 				return inst
 			end
 		end
+		timeOut -= 1
 	end
 end
 
 function instance:GetChildrenOfClass(i: Instance, className : string)
 	local instancesOfClass = {}
 	for _,child in pairs(i:GetChildren()) do
-		if child:IsA(className) then
+		if child.ClassName == className then
 			table.insert(instancesOfClass, child)
 		end
 	end
@@ -32,11 +32,31 @@ end
 function instance:GetDescendantsOfClass(i : Instance, className : string)
 	local descendantsOfClass = {}
 	for _,descendant in pairs(i:GetDescendants()) do
-		if descendant:IsA(className) then
+		if descendant.ClassName == className then
 			table.insert(descendantsOfClass, descendant)
 		end
 	end
 	return descendantsOfClass
+end
+
+function instance:GetChildrenWhichAre(i : Instance, t : string)
+	local instancesWhichAre = {}
+	for _,child in pairs(i:GetChildren()) do
+		if child:IsA(t) then
+			table.insert(instancesWhichAre, child)
+		end
+	end
+	return instancesWhichAre
+end
+
+function instance:GetDescendantsWhichAre(i : Instance, t : string)
+	local descendantsWhichAre = {}
+	for _,descendant in pairs(i:GetDescendants()) do
+		if descendant:IsA(t) then
+			table.insert(descendantsWhichAre, descendant)
+		end
+	end
+	return descendantsWhichAre
 end
 
 function instance:GetDescendants(i : Instance)
@@ -48,6 +68,10 @@ function instance:GetDescendants(i : Instance)
 		end
 	end
 	return descendants
+end
+
+function instance:FindFirstDescendant(i : Instance, name : string)
+	return i:FindFirstChild(name, true)
 end
 
 return instance
